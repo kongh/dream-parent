3b7566ed739c47bf3cf0ecdc5a18e3e9ab4dcd3f
kongh <563796329@qq.com>
2015-10-15 16:24:32 +0800

    实现序列化

--- dream-core/src/main/java/com/coder/dream/base/web/form/BaseForm.java
+++ dream-core/src/main/java/com/coder/dream/base/web/form/BaseForm.java
@@ -1,7 +1,9 @@
 package com.coder.dream.base.web.form;
 
+import java.io.Serializable;
+
 /**
  * Created by Administrator on 2015/8/20.
  */
-public abstract class BaseForm {
+public abstract class BaseForm implements Serializable{
 }
--- dream-core/src/main/java/com/coder/dream/base/web/vo/BaseVo.java
+++ dream-core/src/main/java/com/coder/dream/base/web/vo/BaseVo.java
@@ -1,7 +1,9 @@
 package com.coder.dream.base.web.vo;
 
+import java.io.Serializable;
+
 /**
  * Created by konghang on 15-4-19.
  */
-public abstract class BaseVo {
+public abstract class BaseVo implements Serializable{
 }
